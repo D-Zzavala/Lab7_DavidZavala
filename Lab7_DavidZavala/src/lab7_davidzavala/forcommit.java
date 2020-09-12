88d64d9ed3a7961ed6fa98222ad5923165ba4593
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-09-11 19:19:51 -0600

    bug trying time numero x

---
+++ Lab7_DavidZavala/src/lab7_davidzavala/forcommit.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7_davidzavala;
+
+/**
+ *
+ * @author 50431
+ */
+public class forcommit {
+    
+    private int trying;
+    private int trying2;
+    private int trying3;
+    private int trying4;
+    private int trying6;
+    private int trying7;
+    private int trying9;
+
+    public int getTrying() {
+        return trying;
+    }
+
+    public void setTrying(int trying) {
+        this.trying = trying;
+    }
+
+    public int getTrying2() {
+        return trying2;
+    }
+
+    public void setTrying2(int trying2) {
+        this.trying2 = trying2;
+    }
+
+    public int getTrying3() {
+        return trying3;
+    }
+
+    public void setTrying3(int trying3) {
+        this.trying3 = trying3;
+    }
+
+    public int getTrying4() {
+        return trying4;
+    }
+
+    public void setTrying4(int trying4) {
+        this.trying4 = trying4;
+    }
+
+    public int getTrying6() {
+        return trying6;
+    }
+
+    public void setTrying6(int trying6) {
+        this.trying6 = trying6;
+    }
+
+    public int getTrying7() {
+        return trying7;
+    }
+
+    public void setTrying7(int trying7) {
+        this.trying7 = trying7;
+    }
+
+    public int getTrying9() {
+        return trying9;
+    }
+
+    public void setTrying9(int trying9) {
+        this.trying9 = trying9;
+    }
+    
+    
+}
